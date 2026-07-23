dfe3755fdf2bbfbbce58fbd4a46ad204efadfc99
Masami Ogoshi <ogochan@beesnest-inc.com>
2026-07-23 17:37:01 +0900

    fix: add jspreadsheet CSS imports to wiki-main.js

--- client/src/wiki-main.js
+++ client/src/wiki-main.js
@@ -1,4 +1,6 @@
 import './styles/style.scss';
+import 'jspreadsheet-ce/dist/jspreadsheet.css';
+import 'jspreadsheet-ce/dist/jspreadsheet.themes.css';
 import { mount } from 'svelte';
 import Knowledge from './pages/Knowledge.svelte';
 
